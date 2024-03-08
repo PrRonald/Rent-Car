73c40a00a8b02cc22c2564c932e9afefdb09353e
PrRonald <ronaldne69@gmail.com>
2024-03-07 20:49:18 -0400

    error in a file

--- src/Components/ButtonRent.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export const ButtonRent = () => {
-    return(
-        <button className="border-2 border-blue-500
-                         rounded-lg w-[100px] h-[45px] m-2 ">Rentar</button>
-    );
-}
